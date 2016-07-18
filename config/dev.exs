2834e212053a8fbd0d8c126f93b4ff2363578b03
Sven Bohm <bohms@msu.edu>
2016-07-18 14:30:34 -0400

    use secret credentials

--- config/dev.exs
+++ config/dev.exs
@@ -34,10 +34,4 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
-config :drought, Drought.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "drought_dev",
-  hostname: "localhost",
-  pool_size: 10
+import_config "prod.secret.exs"
