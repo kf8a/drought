38d82237a7bb2109d9a06b3fd88e917211193dc7
Sven Bohm <bohms@msu.edu>
2016-07-20 09:40:55 -0400

    set the schema prefix

--- web/models/drought.ex
+++ web/models/drought.ex
@@ -1,6 +1,8 @@
 defmodule Drought.Drought do
   use Drought.Web, :model
 
+  @schema_prefix 'weather'
+
   schema "drought_index" do
     field :date, Ecto.Date
     field :cumulative_precip_in, :float
